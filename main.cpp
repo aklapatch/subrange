5ad881ae29878dc2d01da6700d99c5447cad0d80
me <me@core>
2018-10-11 21:48:25 -0400

    finish up

--- main.cpp
+++ main.cpp
@@ -1,72 +1,59 @@
 #include<vector>
 #include<iostream>
 
-// represents a range of intengers
+// represents a range of integers
 struct subrange {
-	int start, end, step;
+	int start, end;
 
 	// sets start and end
 	subrange(int start_in, int end_in){
-		step = 1;
-		start = start_in;
-		end = end_in;
-	}
-
-	// sets start, end and step
-	subrange(int start_in, int end_in, int step_in){
-		step = step_in;
 		start = start_in;
 		end = end_in;
 	}
 	
 	// returns vector with the range of values
 	std::vector<int> getRange(){
-		int size = ( (end-start)%step != 0 ) ? (end-start)/step +1 : (end-start)/step;
-		std::vector<int> output(size);
+		std::vector<int> output(end - start + 1);
 
-		int j=0;
-		for(int i = start; i <= end; i+=step){
-			output[j++]=i;
-		}
-		return output;
-	}
-	// return vector with the selected range and step of 1
-	static std::vector<int> get(int start, int end){	
-		std::vector<int> output(end-start + 1);
 		int j=0;
 		for(int i = start; i <= end; ++i){
 			output[j++]=i;
 		}
+
+		return output;
+	}
+
+	// return vector with the selected range and step of 1
+	static std::vector<int> get(int start, int end){	
+		std::vector<int> output(end-start + 1);
+
+		int j=0;
+		for(int i = start; i <= end; ++i){
+			output[j++]=i;
+		}
+
 		return output;
 	}
 };
 
 int main(){
-	subrange test(10,20,4);
 
+	// output subrange to vector
+	subrange test = subrange(19,26);
 	std::vector<int> out = test.getRange();
-	
-	// test getRange method
-	std::cout << "getRange operation: ";
+
+	// output results of constructor
+	std::cout << "two parameter constructor: ";
 	for(int i = 0; i < out.size(); ++i){
 		std::cout << out[i] << "\t";
 	}
 	std::cout << "\n";
 
-	// use other constructor
-	test = subrange(19,26);
-	out = test.getRange();
-
-	// test results of other constructor
-	std::cout << "Testing 2 parameter constructor: ";
-	for(int i = 0; i < out.size(); ++i){
-		std::cout << out[i] << "\t";
-	}
-	std::cout << "\n";
-
-	// test static get method
+	// get results of static method
 	out = subrange::get(34, 42);
 	std::cout << "Static get operation: ";
+
+	// output results
 	for(int i = 0; i < out.size(); ++i){
 		std::cout << out[i]<< "\t";
 	}
